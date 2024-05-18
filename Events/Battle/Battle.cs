41d6ea929d157983b6237b3c6522a4fd40e36331
Orion <orionnye@Orions-MacBook-Pro.local>
2024-05-17 21:52:26 -0700

    added icons from Leif

--- Events/Battle/Battle.cs
+++ Events/Battle/Battle.cs
@@ -1,8 +1,7 @@
 using Godot;
 using System;
 
-public partial class Battle : Node3D
-{	
+public partial class Battle : Node3D {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
 	}
